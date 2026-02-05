2f610d4e49b2895820556e1ce3d8391eb3644db7
BomberMan <175359849+bomber-ban@users.noreply.github.com>
2026-02-05 16:47:24 +0200

    Update endpoints.py

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,6 +1,5 @@
 # /home/xty/Documents/Projects/PetProject/BomberBot_GUI/BomberBot_GUI/api/endpoints.py
-#DOMAIN = "https://bomberbot.cc"
-DOMAIN = "http://127.0.0.1:8000"
+DOMAIN = "https://bomberbot.cc"
 
 SERVICE_STATUS_URL = f"{DOMAIN}/calls/api/endpoints/service/status"
 BALANCE_URL = f"{DOMAIN}/calls/api/endpoints/account/get_balance"
